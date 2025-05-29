820e644deab1ba0341636fa85007d86741630700
ELGUEZABAL SELIOUTIN Dario <dario.elguezabal@soprasteria.com>
2025-05-29 17:55:52 +0200

    Habilito CORS 2

--- src/main/java/com/example/demo2/book/BookController.java
+++ src/main/java/com/example/demo2/book/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "https://spiffy-douhua-1e2552.netlify.app")
 @RestController
 @RequestMapping("api/v1")
 public class BookController {
